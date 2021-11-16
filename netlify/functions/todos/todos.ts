4011d65db385c341d134a8d765592cf8eba05968
Moonpearl <romain.tello@gmail.com>
2021-11-16 11:52:00 +0100

    Add all API routes

--- netlify/functions/todos/todos.ts
+++ netlify/functions/todos/todos.ts
@@ -1,6 +1,9 @@
-import createConnectionPool, { sql } from '@databases/pg';
+import createConnectionPool from '@databases/pg';
+import tables from '@databases/pg-typed';
 import { Handler } from '@netlify/functions'
 import dotenv from 'dotenv';
+import DatabaseSchema, {serializeValue} from '../../../__generated__';
+import Todos from '../../../__generated__/todos';
 
 // Read environment variables from .env file
 dotenv.config();
@@ -13,37 +16,127 @@ const {
   DB_NAME,
 } = process.env;
 
-// GET /todos        => Récupère toutes les tâches à faire
-// GET /todos/:id    => Récupère une tâche à faire en particulier
-// POST /todos       => Créer une tâche à faire
-// PATCH /todos/:id  => Modifier une tâche à faire existante en particulier
-// PUT /todos/:id    => Remplacer une tâche à faire existante en particulier
-// DELETE /todos/:id => Supprimer une tâche à faire existante en particulier
+class NotFoundException extends Error {
+
+}
+
+class MethodNotAllowedException extends Error {
+  public httpMethod: string;
+
+  constructor(httpMethod: string) {
+    super();
+    this.httpMethod = httpMethod;
+  }
+}
 
 export const handler: Handler = async (event, context) => {
-  // event.body   // Récupère le corps de la requête
-  // event.httpMethod   // Récupère la méthode HTTP de la requête
-  // event.path   // Récupère l'URL demandée par l'utilisateur
-
   // Establish connection with database
   const db = createConnectionPool(
-    `postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+    `postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
   );
 
-  // Send SQL query to database
-  const results = await db.query(sql`
-    SELECT * FROM todos
-  `);
+  // Get todos handler
+  const { todos } = tables<DatabaseSchema>({
+    serializeValue,
+  });
 
-  // Terminate connection with database
-  await db.dispose();
-  
-  // Create HTTP response
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(results),
+  try {
+    let match: RegExpMatchArray;
+    let result: Todos[] | Todos;
+    let statusCode = 200;
+    // If lambda was called without an ID
+    if (event.path.match(/^\/\.netlify\/functions\/[^\/]+$/)) {
+      switch (event.httpMethod) {
+        // Find all
+        case 'GET':
+          result = await todos(db).find().all();
+          break;
+
+        // Create
+        case 'POST':
+          result = (await todos(db).insert(JSON.parse(event.body)))[0];
+          statusCode = 201;
+          break;
+
+        // Method not allowed
+        default:
+          throw new MethodNotAllowedException(event.httpMethod);
+      }
+
+    // If lambda was called with an ID
+    } else if (match = event.path.match(/^\/\.netlify\/functions\/[^\/]+\/(\d+)$/)) {
+      const id = Number(match[1]);
+      switch (event.httpMethod) {
+        // Find by ID
+        case 'GET':
+          result = await todos(db).findOne({ id });
+          console.log(result);
+          if (result === null) {
+            throw new NotFoundException();
+          }
+          break;
+
+        // Update
+        case 'PATCH':
+        case 'PUT':
+          result = (await todos(db).update({ id }, JSON.parse(event.body)))[0];
+          if (typeof result === 'undefined') {
+            throw new NotFoundException();
+          }
+          break;
+
+        // Delete
+        case 'DELETE':
+          result = await todos(db).findOne({ id });
+          if (result === null) {
+            throw new NotFoundException();
+          } else {
+            await todos(db).delete({ id });
+            statusCode = 204;
+            result = null;
+          }
+          break;
+
+        // Method not allowed
+        default:
+          throw new MethodNotAllowedException(event.httpMethod);
+      }
+    
+    // If lambda was called with inappropriate arguments
+    } else {
+      throw new NotFoundException();
+    }
+    // Terminate connection with database
+    await db.dispose();
+    
+    // Create HTTP response
+    return {
+      statusCode,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: result === null ? '' : JSON.stringify(result),
+    }
+  }
+  catch (error) {
+    if (error instanceof NotFoundException) {
+      return {
+        statusCode: 404,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ error: 'Resrouce not found.' })
+      }
+    }
+    if (error instanceof MethodNotAllowedException) {
+      return {
+        statusCode: 405,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ error: `Method ${error.httpMethod} not allowed.` })
+      }
+    }
+    throw error;
   }
 }
